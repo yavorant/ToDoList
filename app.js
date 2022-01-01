530a01d2cc3013a54b3619e85adb8eb0a5cebc22
yavorant <yavorant@gmail.com>
2022-01-01 17:46:20 +0200

    push works

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 // alert('it works!');
 
 let userInput = prompt("What would you like to do?");
-while(true) {
-  userInput = prompt("What would you like to do?");
-  
-  // if (userInput === 'new') {
-  //   let newTodo = prompt("Add a new todo");
-  //   console.log('Create a todo!');
-  //   todos = todos.push(newTodo);
-  // } else if (userInput === 'list') {
-  //   console.log('List todos!');
-  // } else if (userInput === 'delete') {
-  //   console.log('Delete a todo!');
-  // } else 
-  if (userInput === 'quit') {
+let todos = [];
+while(true) {  
+  if (userInput === 'new') {
+    let newTodo = prompt("Add a new todo");
+    console.log('Create a todo!');
+    todos.push(newTodo);
+    console.log('newTodo: ', newTodo);
+    console.log('todos: ', todos);
+
+  } else if (userInput === 'list') {
+    console.log('List todos: ', todos);
+  } else if (userInput === 'delete') {
+    console.log('Delete a todo!');
+  } else if (userInput === 'quit') {
     break;
   }  
+  userInput = prompt("What would you like to do?");
 }
   
 console.log('The End');
