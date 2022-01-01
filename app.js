083685d0448af2fdf6ef09ddf2149bbbdb57dda2
yavorant <yavorant@gmail.com>
2022-01-01 18:28:49 +0200

    Complete

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 // alert('it works!');
 
 let userInput = prompt("What would you like to do?");
-let todos = [];
+let todos = ['dog', 'cat', 'man'];
 while(true) {  
   if (userInput === 'new') {
-    let newTodo = prompt("Add a new todo");
-    console.log('Create a todo!');
+    let newTodo = prompt(' Add a new todo! ');
+    console.log('***** Add a new todo *****');
     todos.push(newTodo);
-    console.log('newTodo: ', newTodo);
-    console.log('todos: ', todos);
+    console.log(`"${newTodo}" added to the list`);
+    // console.log('todos: ', todos);
 
   } else if (userInput === 'list') {
-    console.log('List todos: ', todos);
+    console.log('***** List *****');
+    for (let i = 0; i < todos.length; i++) { 
+      console.log(` ${i} : ${todos[i]}`)
+    }
   } else if (userInput === 'delete') {
-    console.log('Delete a todo!');
+    console.log('***** WARNING a todo item will be deleted!!! *****');
+    let index = prompt(' Enter an index of element that will be deleted ');
+    let deletedItem = todos[index];
+    todos.splice(index, 1);
+    console.log(`Unfortunately the "${deletedItem}" item was deleted forever :(`);
   } else if (userInput === 'quit') {
     break;
   }  
